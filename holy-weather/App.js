e2e9dd845e0c8867417faf75aff8da345250b95a
abelko <esung1129@gmail.com>
2018-03-23 02:32:45 +0900

    react-native weather app 2-2 React Native Layouts with Flexbox

---
+++ holy-weather/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    flexWrap:"wrap"
+  },
+  redView: {
+    height:50,
+    width:50,
+    backgroundColor:'red',
+  },
+  yellowView: {
+    height:50,
+    width:50,
+    backgroundColor:'yellow',
+  }
+});
