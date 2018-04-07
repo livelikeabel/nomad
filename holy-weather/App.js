76ce9661f76facf123ce6bb684216085f6728afc
abelko <esung1129@gmail.com>
2018-04-07 11:31:12 +0900

    Building the Loading View

--- holy-weather/App.js
+++ holy-weather/App.js
@@ -1,28 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the holy weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -31,20 +22,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'space-around',
-    alignItems: 'flex-end',
-    flexDirection: 'row',
-    flexWrap:"wrap"
+    backgroundColor: '#fff'
   },
-  redView: {
-    height:50,
-    width:50,
-    backgroundColor:'red',
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: "flex-end",
+    paddingLeft: 25
   },
-  yellowView: {
-    height:50,
-    width:50,
-    backgroundColor:'yellow',
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
   }
 });
